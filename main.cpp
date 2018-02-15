3d8abb6ad266dfd32564946e48a2c23f7cfc6c4c
Justin <jwiegel2@wisc.edu>
2018-02-15 01:30:49 -0600

    Cleaned up card select function and added number of players

--- main.cpp
+++ main.cpp
@@ -43,47 +43,54 @@ class Player
 	void selectCard(){
 		// Wait for player to type valid card name, then allow options depending
 		// on which card was selected
-		// TODO: Make it so player can't type literally any card
 		bool validCard = false;
 		while(!validCard){
 			std::string input;
 			std::cin >> input;
 			std::transform(input.begin(), input.end(), input.begin(), ::tolower);
 			if(input.compare("guard") == 0){
+				validCard = checkValid(1);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("priest") == 0){
+				validCard = checkValid(2);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("baron") == 0){
+				validCard = checkValid(3);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("handmaid") == 0){
+				validCard = checkValid(4);
 				std::cout << "Select target player: ";
-				validCard = true;
-				isProtected = true;
 			}
 			else if(input.compare("prince") == 0){
+				validCard = checkValid(5);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("king") == 0){
+				validCard = checkValid(6);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("countess") == 0){
+				validCard = checkValid(7);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else if(input.compare("princess") == 0){
+				validCard = checkValid(8);
 				std::cout << "Select target player: ";
-				validCard = true;
 			}
 			else std::cout << "Enter the name of a card in your hand.\n";
 		}
+	}
+
+	bool checkValid(int card){
+		if(hand.front() == card || hand.back() == card){
+			if(hand.front() == card) hand.pop_front();
+			else hand.pop_back();
+			return true;
+		}
+		else return false;
 	}
 
 	public:
@@ -107,6 +114,31 @@ class Player
 };
 
 int main() {
+	// Determine if user inputted number of players is valid
+	bool valid = false;
+	std::string input = "";
+	int numPlayers = 0;
+
+	while(!valid){
+		std::cout << "Please enter the number of players (2-4): ";
+		std::getline(std::cin, input);
+
+		if(input.compare("2") == 0){
+			numPlayers = 2;
+			valid = true;
+		}
+		else if(input.compare("3") == 0){
+			numPlayers = 3;
+			valid = true;
+		}
+		else if(input.compare("4") == 0){
+			numPlayers = 4;
+			valid = true;
+		}
+		else std::cout << "Enter a number between 2 and 4.\n";
+	}
+	
+	
 	// Generate random number to shuffle deck
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 
